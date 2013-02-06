298777d5f9d58ee15c3d1b97dfa5d7209c5f7282
mstyn <mst.tyn@gmail.com>
2013-02-06 15:07:42 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
-var express = require('express');
+﻿//Modules
+var express = require("express");
 var app = express();
+var MongoStore = require('connect-mongo')(express);
+var dbsettings = require('./config').settings; //Server configuration
+global.saldo = 'fewfwef352tFRWEQF';
+global.controllers = require('./controllers'); //Controllers
+var routers = require('./routers'); //Routers
+var viewEngine = 'ejs'; 
 
-app.configure(function() {
-    app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
-    app.set('view engine', 'html')                      
-    app.set('views', __dirname + "/tpl");
-    app.set("view options", {layout: 'layout.html'});   // Файл layout.html по умолчанию будет оборачивать все шаблоны
-    app.use(express.static(__dirname + "/public"));     // Делаем файлы из папки public доступными на сайте
-});
-
-app.get('/', function(req, res){          // Обрабатываем запрос корневой страницы "/"
-    res.render('index.html');
-	
-});
-app.get('/profile', function(req, res){ // Обрабатываем запрос страницы "/profile"
-    res.render('profile.html');
-	
-});
-app.get('/registr', function(req, res){ // Обрабатываем запрос страницы "/profile"
-    res.render('registr.html');
-	
-});
-app.get('/entry', function(req, res){ // Обрабатываем запрос страницы "/profile"
-    res.render('entry.html');
-	
-});
-app.get('/portfolio', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
-    res.render('portfolio.html');
-	
-});
-app.get('/sethelp', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
-    res.render('sethelp.html');
-	
-});
-app.get('/gethelp', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
-    res.render('gethelp.html');
-	
-});
-app.get('/structure', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
-    res.render('structure.html');
-	
-});
-app.get('/history', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
-    res.render('history.html');
-	
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', viewEngine);
+  /*---------------------------------------------------*/
+  app.use(express.cookieParser());
+  app.use(express.session({ 
+      secret: 'fegwegwe',
+      store: new MongoStore(dbsettings),
+      cookie: { path: '/', httpOnly: true, maxAge: 1000*60*60*24 }
+    }));
+  /*---------------------------------------------------*/
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-var port = process.env.PORT || 5000;       
-app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
-console.log("Listening at " + port)        // Пишем в консоль, что запустились
+app.get('/',routers.index); //Стартовая страница
+app.post('/auth/register',routers.auth.register); //Регистрация пользователей
+app.post('/auth',routers.auth.login); //Вход на сайт
+app.get('/auth',routers.auth.getlogin); //Проверить состояния входа: залогинился или нет
+app.del('/auth',routers.auth.logout); //Выход с сайта
+
+//Connect to db and start
+global.controllers.db.opendb(dbsettings, function(error,db){
+    if (!error){
+        global.db = db;
+        app.listen(process.env.PORT);
+    } else console.log('Error connect to db');
+});
