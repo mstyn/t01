bfb8ebd3c10b71047915b0f62c00ad51dcd324e9
mstyn <mst.tyn@gmail.com>
2013-02-06 15:37:42 +0700

    initial commit

---
+++ controllers/stayonlinesessions.js
@@ -0,0 +1,57 @@
+﻿// Если пользователь ставит галочку «запомнить меня» мы будем добавлять в куки хешированный идентификатор пользователя и хранить // все будем в контроллере документов «stayonlinesessions»
+
+exports.savesession = function(user_id, callback){
+
+    global.db.collection('stayonlinesessions', function(err, collection) {
+        if (err){
+            callback(true);
+            return;
+        }
+        var hash = global.controllers.db.criptpassword(user_id.toString());
+        collection.insert({user_id: user_id, hash: hash, createdate: new Date()},{safe:true},function(error, result){
+            if (err){
+                callback(true);
+                return;
+            }   
+            callback(false, hash);
+        });
+    });    
+};
+
+exports.getsession = function(hash, callback){
+    global.db.collection('stayonlinesessions', function(err, collection) {
+        if (err){
+            callback(true);
+            return;
+        }
+        collection.findOne({hash: hash}, function(error,item){
+            if (err || item === null){
+                callback(true);
+                return;
+            }   
+            global.controllers.users.getuser(item.user_id, function(error, user){
+                if (err || user === null){
+                    callback(true);
+                    return;
+                }
+                callback(false, user);
+            });
+        });        
+    });   
+};
+
+exports.delsession = function(hash, callback){
+    global.db.collection('stayonlinesessions', function(err, collection) {
+        if (err){
+            callback(true);
+            return;
+        }        
+        collection.remove({hash: hash}, {safe: true}, function(err,removed){
+            if (err || !removed){
+                callback(true);
+                return;
+            }   
+            callback(false);
+        });        
+    }); 
+};
