4f475c66e4afa54850e1080e5f5189a9c8c731b1
mstyn <mst.tyn@gmail.com>
2013-02-06 15:52:15 +0700

    initial commit

--- controllers/db.js
+++ controllers/db.js
@@ -3,12 +3,15 @@
       Server = mongo.Server,
       Db = mongo.Db;
     
-    var server = new Server(settings.host, settings.port, {auto_reconnect: settings.auto_reconnect});
-    var db = new Db(settings.db, server);
+    /* var server = new Server(settings.host, settings.port, {auto_reconnect: settings.auto_reconnect});
+    var db = new Db(settings.db, server); */
+	
+	var server = new Server('linus.mongohq.com', 10050, {auto_reconnect: settings.auto_reconnect});
+    var db = new Db('my01', server);
     
     db.open(function(err, db) {
       if(!err) {
-        db.authenticate(settings.username, settings.password, function(){callback(false, db);});
+        db.authenticate('mstyn', 'mstyn04', function(){callback(false, db);});
       } else callback(true, db);
     });    
 };
